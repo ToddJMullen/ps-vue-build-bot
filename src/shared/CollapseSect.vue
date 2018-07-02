3f42171de193cc7381007598a6fb992ab3f21c01
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-07-02 07:22:53 -0400

    Define new CollapseSect component for hiding/showing markup

---
+++ src/shared/CollapseSect.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<div class="header">
+			<span v-if="open" @click="open = !open">{{titleOpen}} Collapse &#x25B2;</span>
+			<span v-if="!open" @click="open = !open">{{titleClosed}} Expand &#x25BC;</span>
+		</div>
+		<slot v-if="open">
+		<!-- injected content here -->
+			<div>No Content Was Wrapped! (this is component filler)</div>
+		</slot>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "CollapseSect"
+	,data(){
+		return {
+			open: true
+		}
+	}
+	,props: {
+		titleOpen: {type: String, required: true}
+		,titleClosed: {type: String, required: true}
+	}
+}
+</script>
+
+<style scoped>
+	.header{
+		text-align: right;
+		padding: 3px;
+		background-color: #bbb;
+		cursor: pointer;
+	}
+</style>
