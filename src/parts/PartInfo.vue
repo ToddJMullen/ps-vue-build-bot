3f0d2f09fd034825ce17e7b3033ac6ed909b1cfc
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-07-03 17:15:39 -0400

    Add base PartInfo component

---
+++ src/parts/PartInfo.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<div>
+			<h1>{{ part.title }}</h1>
+		</div>
+		<div>
+			{{ part.description }}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "PartInfo"
+	,data(){
+		return {
+			part: {
+				title: "Part Title"
+				,description: "Part Description"
+			}
+		}
+	}
+}
+</script>
+
+<style scoped >
+
+</style>
