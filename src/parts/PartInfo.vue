fed9d064edd6cabe08cf252ee27bf17fb74bf71c
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-07-03 21:01:48 -0400

    PartInfo, Add dynamic part info using given path params - Replace hard coded data().part info with computer.part()

--- src/parts/PartInfo.vue
+++ src/parts/PartInfo.vue
@@ -10,14 +10,24 @@
 </template>
 
 <script>
+import parts from "../data/parts";
+
 export default {
 	name: "PartInfo"
+	,computed: {
+		part(){
+			// let id		= this.$route.params.id
+			// ,partType	= this.$route.params.partType
+			// ;//old way
+			let { partType, id } = this.$route.params;
+			//ES2015, using destructuring
+
+			// return parts[ type ][ id ];//wrong struct?
+			return parts[ partType ].find( part => part.id === +id );
+		}
+	}
 	,data(){
 		return {
-			part: {
-				title: "Part Title"
-				,description: "Part Description"
-			}
 		}
 	}
 }
