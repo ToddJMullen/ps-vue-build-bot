7a777c4047718318ca9f7bf0fc18fb38c3b05645
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-07-04 00:04:24 -0400

    Add base SidebarBuild & SidebarStandard components

---
+++ src/sidebars/SidebarBuild.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="sidebar">
+		Build Sidebar
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Build"
+};
+</script>
+
+<style scoped>
+.sidebar{
+	font-size: 50px; color: #3f3;
+	transform: rotate(90deg);
+}
+</style>
+
+
