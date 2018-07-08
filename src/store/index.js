a3dc96c7906b73c23794036f983adc51bd21a877
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-07-07 21:54:16 -0400

    $store, Import new users store module

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import moduleRobots from "./modules/robots";
+import moduleUsers	from "./modules/users";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
 	modules: {
 		robots: moduleRobots
+		,users: moduleUsers
 	}
 });
