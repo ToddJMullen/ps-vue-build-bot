b8d0867021e55f618846a94655528bf73c7a00c9
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-07-07 21:00:24 -0400

    Begin dividing $store into modules. - Define store/modules/robots.js -  Move Vuex.Store() implementation into robots.js  module default export - Import robots module into $store modules definition

--- src/store/index.js
+++ src/store/index.js
@@ -1,43 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import axios from "axios";
+
+import moduleRobots from "./modules/robots";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	state: {
-		cart: []//create initial object so that Vue will know to set up watchers
-		,parts: null
-	}
-	,mutations: {//all "direct" changes must/should be through mutation methods
-		//mutations are accessed via $store.commit("methodName", params )
-		addBotToCart( state, bot ){
-			state.cart.push( bot );
-		}
-		,updateParts( state, parts ){
-			console.log("updateParts() setting parts:", parts );
-			state.parts = parts;
-		}
-	}
-	,getters: {
-		cartSaleItems(state){
-			return state.cart.filter( item => item.head.onSale );
-		}
-	}
-	,actions: {//actions are accessed via $store.dispatch("methodName", params )
-		getParts({commit}){ //destructure from context input which contains: state, getters, commit, dispatch
-			// axios.get("http://localhost:8081/api/parts");
-			// ^^^^ this will cause usual CORS problem, so we create a Vue proxy to treat as relative
-			console.log("getParts() calling service...")
-			axios.get("/api/parts")
-				.then( parts => commit("updateParts", parts.data ) )
-				.catch( console.error );
-		}
-		,addBotToCart({commit, state}, bot ){
-			const cart = [...state.cart, bot ];
-			return axios.post("/api/cart", cart )//add on server
-						.then( () => commit("addBotToCart", bot ) )//when done, add locally too
-						.catch( console.error );
-		}
+
+	modules: {
+		robots: moduleRobots
 	}
 });
