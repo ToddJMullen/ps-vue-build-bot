288c837d94bbcb46bf33e23a7a6af773aec49199
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-07-29 22:12:31 -0400

    pin-directive.js, Change pin directive to use bind & update hooks with an abstracted method - Add other method hook stubs for note purposes

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,21 +1,42 @@
+function setStyle( elem, binding ){
+    console.log("setStyle(), binding:", binding );
+    elem.style.position = "absolute";
+    Object.keys(binding.value).map( position => {
+	elem.style[ position ] = binding.value[ position ];
+    });
+}
+
 export default {
 
-    bind: (elem, binding ) => {//bind is called as soon as directive is bound to element
-	console.log("binding:", binding );
+    bind: (elem, binding) => {
+	setStyle( elem, binding );
+    }
+    ,update: (elem, binding ) => {//bind is called as soon as directive is bound to element
+	setStyle( elem, binding );
+//	console.log("binding:", binding );
 //	if( binding.arg !== "position"){//then it's unknown
 //	    return;
 //	}
-	// ^^^ binding.arg is used with the "v-pin.absolute..." syntax
-	elem.style.position = "absolute";
-	Object.keys(binding.value).map( position => {
-	    elem.style[ position ] = binding.value[ position ];
-	});
+//	// ^^^ binding.arg is used with the "v-pin.absolute..." syntax
+//	elem.style.position = "absolute";
+//	Object.keys(binding.value).map( position => {
+//	    elem.style[ position ] = binding.value[ position ];
+//	});
 //	Object.keys(binding.modifiers).map( key => {
 //	    elem.style[ key ] = "5px";
 //	});
 	// ^^^ binding.modifiers is used with the "v-pin.absolute.top.right" syntax
 //	elem.style.bottom = "5px";
 //	elem.style.right = "5px";
+    }
+    ,inserted: (elem, binding ) => {
+	console.log("...node inserted into parent")
+    }
+    ,componentUpdated: (elem, binding ) => {
+//	fires like updated, but only after all children updated
+    }
+    ,unbind: (elem, binding ) => {
+	//called when unbound from parent component
     }
 
 }//pinDirective
